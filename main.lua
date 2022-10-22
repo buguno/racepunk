73862f6c25355268f0afaea07e71b839024129e1
buguno <brunnodesouzabezerra@gmail.com>
2022-10-21 23:44:25 -0300

    feat: drawing car

--- main.lua
+++ main.lua
@@ -1,8 +1,12 @@
 function love.load()
+    x = 370
+    y = 450
     background_image = love.graphics.newImage("misc/images/background_test.png")
+    car = love.graphics.newImage("misc/images/car.png")
 end
 
 
 function love.draw()
     love.graphics.draw(background_image)
+    love.graphics.draw(car, x, y)
 end
